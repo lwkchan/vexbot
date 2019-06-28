8690cc2234d75bc47b657b6f122afea605734f6d
Laura Chan <laura.chan@fundingoptions.com>
2019-06-28 14:07:12 +0800

    fix: finish with simple thing

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ import { physics } from 'popmotion';
 import styler from 'stylefire';
 
 const mainEl = document.getElementById('svg-canvas');
-
 const colors = ['#6007B3', '#FFB33D', '#9323FF', '#08CC4C', '#36B361'];
 
 function getVectorLength({ a, b }) {
@@ -11,13 +10,13 @@ function getVectorLength({ a, b }) {
   return Math.sqrt(xLength * xLength + yLength * yLength);
 }
 
-let printed = false;
 function drawLines() {
   return window
-    .fetch('https://api.noopschallenge.com/vexbot?count=2')
+    .fetch('https://api.noopschallenge.com/vexbot?count=20')
     .then(response => response.json())
     .then(data => data)
     .then(({ vectors }) => {
+      generationCounter++;
       const color = colors[Math.floor(Math.random() * colors.length)];
       vectors.forEach(vector => {
         const vectorEl = document.createElementNS('http://www.w3.org/2000/svg', 'line');
@@ -28,43 +27,24 @@ function drawLines() {
         vectorEl.setAttribute('stroke', color);
         mainEl.append(vectorEl);
 
-        const vectorLength = getVectorLength(vector);
+        const polygonEl = document.createElementNS('http://www.w3.org/2000/svg', 'line');
+        polygonEl.setAttribute('points', `${vector.a.x}, ${vector.a.y} ${vector.b.x}, ${vector.b.y}`);
+
         const vectorStyler = styler(vectorEl);
 
-        // The higher point falls
-        let movingY = 'y1';
-        let movingX = 'x1';
-        let startingX = vector.a.x;
-        let startingY = vector.a.y;
-        let anchorX = vector.b.x;
-        let anchorY = vector.b.y;
-        if (vectorEl.getAttribute('y1') > vectorEl.getAttribute('y2')) {
-          movingY = 'y2';
-          movingX = 'x2';
-          startingX = vector.b.x;
-          startingY = vector.b.y;
-          anchorX = vector.a.x;
-          anchorY = vector.a.y;
-        }
-
-        const falling = physics({ acceleration: 400, restSpeed: false }).start(v => {
-          const newY = startingY + v;
-          const x = vectorLength * vectorLength - (newY - anchorY) * (newY - anchorY);
-          const newX = Math.sqrt(x < 0 ? x * -1 : x) + anchorX;
-          vectorStyler.set({ [movingY]: newY, [movingX]: newX });
+        const falling = physics({ acceleration: 300 }).start(v => {
+          vectorStyler.set({ y1: `${vector.a.y + v}`, y2: `${vector.b.y + v}` });
+          if (vectorStyler.get('y1') > 100000) {
+            falling.stop();
+          }
         });
-
-        if (vectorStyler.get('x1') === vectorStyler.get('x2')) {
-          console.log('stopping');
-          falling.stop();
-        }
       });
     });
 }
 
 function init() {
   drawLines();
-  // window.setInterval(drawLines, 2000);
+  window.setInterval(drawLines, 2000);
 }
 
 init();
